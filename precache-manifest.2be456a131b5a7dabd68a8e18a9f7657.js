54f05eafa17e15f602a979b5d6352c0ceef38138
max <maxaqquepucho@gmail.com>
2019-10-07 09:50:43 -0500

    Updates

---
+++ precache-manifest.2be456a131b5a7dabd68a8e18a9f7657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd165d1d51c0b3910145e8eb222977a",
+    "url": "/aimo-prueba-react/index.html"
+  },
+  {
+    "revision": "448fefaa2b4472fbaf6f",
+    "url": "/aimo-prueba-react/static/css/main.6657b568.chunk.css"
+  },
+  {
+    "revision": "bba3730fc2c83958a1a8",
+    "url": "/aimo-prueba-react/static/js/2.54fe675d.chunk.js"
+  },
+  {
+    "revision": "448fefaa2b4472fbaf6f",
+    "url": "/aimo-prueba-react/static/js/main.4d6c8a60.chunk.js"
+  },
+  {
+    "revision": "f426de919ff9a31a1362",
+    "url": "/aimo-prueba-react/static/js/runtime-main.adf1d85f.js"
+  },
+  {
+    "revision": "0870e4a3e919d59ca828ae8af43f87b3",
+    "url": "/aimo-prueba-react/static/media/binoculars.0870e4a3.svg"
+  }
+]);
